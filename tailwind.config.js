5b1cc25789d4f8b5c64838213dd33cc560947806
nao-sakura <osawa_nao@sakuratravel.jp>
2023-05-17 13:36:13 +0900

    First commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,4 +14,16 @@ module.exports = {
     './dist/**/*.{js,jsx,ts,tsx,vue,html}',
     "./src/**/*.{html,js}",
   ],
+
+  theme: {
+    extend: {
+      colors: {
+        "White": "#ffffff",
+        "Primery": "#FF5890",
+        "Primery-back": "#FFF0F5",
+        "Text-black": "#232323",
+        "Text-link": "#386FFC",
+      },
+    },
+  },
 }
